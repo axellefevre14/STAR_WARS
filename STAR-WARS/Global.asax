75671fa821096314eebcd4923682513eaa4c7977
Axel <axel@live.fr>
2019-02-22 10:01:46 +0100

    Init commit

---
+++ STAR-WARS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="STAR_WARS.MvcApplication" Language="C#" %>
---
+++ STAR-WARS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(STAR_WARS.Startup))]
+namespace STAR_WARS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
