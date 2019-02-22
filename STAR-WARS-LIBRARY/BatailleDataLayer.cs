b6c44f90fe2eef6a858ac6d1b403525e10c0af00
Thomas <thomastiercin@gmail.com>
2019-02-22 14:10:17 +0100

    DataLayer Combat et Bataille

---
+++ STAR-WARS-LIBRARY/BatailleDataLayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STAR_WARS_LIBRARY
+{
+    public class BatailleDataLayer
+    {
+        public List<Bataille> getAll()
+        {
+            List<Bataille> list;
+            using (BDDEntities context = new BDDEntities())
+            {
+                list = context.Bataille.Include("Planete").ToList();
+            }
+            return list;
+        }
+
+        public void add(Bataille bataille)
+        {
+            using (BDDEntities context = new BDDEntities())
+            {
+                context.Bataille.Add(bataille);
+                context.SaveChanges();
+            }
+        }
+        public void update(Bataille bataille)
+        {
+            using (BDDEntities context = new BDDEntities())
+            {
+                context.Bataille.Add(bataille);
+                context.Entry(bataille).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public Bataille getById(int id)
+        {
+            Bataille uneBataille = new Bataille();
+            using (BDDEntities context = new BDDEntities())
+            {
+                uneBataille = context.Bataille.Find(id);
+            }
+            return uneBataille;
+        }
+    }
+}
