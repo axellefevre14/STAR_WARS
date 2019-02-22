77c480708ce65adf06cb8288b6310d7d98ca9f13
Thomas <thomastiercin@gmail.com>
2019-02-22 11:02:37 +0100

    Ajout Droide, Game, Planete, Type Controller

---
+++ STAR-WARS/Controllers/DroideController.cs
@@ -0,0 +1,47 @@
+﻿using STAR_WARS_LIBRARY;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace STAR_WARS.Controllers
+{
+    public class DroideController : Controller
+    {
+        private DroideDataLayer _dataLayer = new DroideDataLayer();
+        private TypeDataLayer _dataLayerType = new TypeDataLayer();
+        // GET: Droide
+        public ActionResult Index()
+        {
+            return View(_dataLayer.getAll());
+        }
+        public ActionResult Create()
+        {
+            this.ViewBag.ListType = _dataLayerType.getAll();
+            return this.View();
+        }
+        // POST : Droide
+        [HttpPost]
+        public ActionResult Create(Droide droide)
+        {
+            _dataLayer.add(droide);
+            this.ViewBag.ListType = _dataLayerType.getAll();
+            return this.View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Droide unDroide = _dataLayer.getById(id);
+            this.ViewBag.ListType = _dataLayerType.getAll();
+            return this.View();
+        }
+        [HttpPost]
+        public ActionResult Edit(Droide droide)
+        {
+            _dataLayer.update(droide);
+            this.ViewBag.ListType = _dataLayerType.getAll();
+            return this.View();
+        }
+    }
+}
---
+++ STAR-WARS/Controllers/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace STAR_WARS.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Fight(int nombreWookie, int nombreDroide, string planete)
+        {
+            ActionResult result = this.RedirectToAction("Index", "Login");
+            if (this.Session["USER_LOGIN"] != null && !string.IsNullOrEmpty(this.Session["USER_LOGIN"].ToString()))
+            {
+                result = this.View();
+            }
+            return result;
+
+        }
+    }
+}
