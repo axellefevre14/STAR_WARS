4b60cd084ab1c9a6a8123b3576e90b0fd99f3ceb
Axel <axel@live.fr>
2019-02-22 10:32:51 +0100

    Ajout des DataLayers

---
+++ STAR-WARS-LIBRARY/WookieDataLayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STAR_WARS_LIBRARY
+{
+    class WookieDataLayer
+    {
+
+        public List<Wookie> getAll()
+        {
+            List<Wookie> list;
+            using (BDDEntities context = new BDDEntities())
+            {
+                list = context.Wookie.Include("Planete").ToList();
+            }
+            return list;
+        }
+
+        public void add(Wookie wookie)
+        {
+            using (BDDEntities context = new BDDEntities())
+            {
+                context.Wookie.Add(wookie);
+                context.SaveChanges();
+            }
+        }
+        public void update(Wookie wookie)
+        {
+            using (BDDEntities context = new BDDEntities())
+            {
+                context.Wookie.Add(wookie);
+                context.Entry(wookie).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public Wookie getById(int id)
+        {
+            Wookie unWookie = new Wookie();
+            using (BDDEntities context = new BDDEntities())
+            {
+                unWookie = context.Wookie.Find(id);
+            }
+            return unWookie;
+        }
+        
+
+    }
+}
